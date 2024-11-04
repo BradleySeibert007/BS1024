9ae8775f4dbee71a0dec0adafd5b430f0e2df47e
BradleySeibert007 <snibot@yahoo.com>
2024-11-04 15:56:21 -0600

    better implementation of new classes with failing results

--- CheckoutTest.java
+++ CheckoutTest.java
@@ -22,47 +22,38 @@ public class CheckoutTest {
             c.main(args);
             bCorrect = HolidayRemoved(false, 1);
             assert(bCorrect);
-            bCorrect = false;
             bCorrect = WeekendRemoved(true, 3, 0);
             assert(bCorrect);
 
             args = new String[]{"CHNS","5","25","7/2/15"};
             c = new Checkout();
             c.main(args);
-            bCorrect = false;
             bCorrect = HolidayRemoved(true, 0);
             assert(bCorrect);
-            bCorrect = false;
             bCorrect = WeekendRemoved(false, 5, 2);
             assert(bCorrect);
             
             args = new String[]{"JAKD","6","0","9/3/15"};
             c = new Checkout();
             c.main(args);
-            bCorrect = false;
             bCorrect = HolidayRemoved(false, 1);
             assert(bCorrect);
-            bCorrect = false;
             bCorrect = WeekendRemoved(false, 6, 2);
             assert(bCorrect);
 
             args = new String[]{"JAKR","9","0","7/2/15"};
             c = new Checkout();
             c.main(args);
-            bCorrect = false;
             bCorrect = HolidayRemoved(false, 1);
             assert(bCorrect);
-            bCorrect = false;
             bCorrect = WeekendRemoved(false, 9, 3);
             assert(bCorrect);
  
             args = new String[]{"JAKR","4","50","7/2/20"};
             c = new Checkout();
             c.main(args);
-            bCorrect = false;
             bCorrect = HolidayRemoved(false, 1);
             assert(bCorrect);
-            bCorrect = false;
             bCorrect = WeekendRemoved(false, 4, 2);
             assert(bCorrect);
             }
@@ -74,10 +65,13 @@ public class CheckoutTest {
                     System.out.println("Test: Holidays to remove count " + Integer.toString(iDaysRemoved));
                     if(iDaysRemoved == iDayValidation){
                         bCorrect = true;
+                    }else{
+                        bCorrect = false;
                     }
                 } catch (ParseException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
+                    bCorrect = false;
                 }
                 return bCorrect;
             }
@@ -87,6 +81,8 @@ public class CheckoutTest {
                 System.out.println("Test: Weekend Days to remove count " + Integer.toString(iTestDays));
                 if(iDaysRented == iValidation){
                     bCorrect = true;
+                }else{
+                    bCorrect = false;
                 }
     
                 return bCorrect;
