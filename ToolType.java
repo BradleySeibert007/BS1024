7714b6d51c7a7343f80d0321d52a647fe352263a
BradleySeibert007 <snibot@yahoo.com>
2024-11-06 09:17:19 -0600

    Update class so that values are private as intended

--- ToolType.java
+++ ToolType.java
@@ -1,9 +1,9 @@
 import java.math.BigDecimal;
 class ToolType {
-    public Boolean bolWeekdayCharge = true;
-    public Boolean bolWeekendCharge = false;
-    public Boolean bolHolidayCharge = false;
-    public BigDecimal bdDailyCharge;
+    private Boolean bolWeekdayCharge = true;
+    private Boolean bolWeekendCharge = false;
+    private Boolean bolHolidayCharge = false;
+    private BigDecimal bdDailyCharge;
     void constructor(String sType) {
 
     }
